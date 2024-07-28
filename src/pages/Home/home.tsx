ef03cc0d46c298adc84fa8c9a51a3f754e0f1203
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-28 17:48:29 -0300

    Add: Home: FullName Input

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -30,6 +30,20 @@ export default function Home() {
   return (
     <section className="w-full h-screen p-2 flex flex-col gap-20">
       <form className='w-1/2 flex flex-col items-center gap-3.5' onSubmit={onSubmit}>
+
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="fullName" className="text-3xl">Nombre y apellido:</label>
+          <input
+            id="fullName"
+            className="block w-full p-2.5 text-2xl bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            type="text"
+            {...register('fullName', { required: true, maxLength: 30, pattern: /^[A-Za-záéíóúÁÉÍÓÚ\s]+$/ })}
+            placeholder="Jon Doe"
+          />
+          {errors.fullName && (
+            <p className="text-sm text-red-500">Falta ingresar el nombre y el apellido.</p>
+          )}
+        </div>
     </section>
   );
 }
