c56ff83f879b1caaa5b395257cfbeb5358ac1efb
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-30 16:46:24 -0300

    Add: CVInput

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -108,6 +108,16 @@ export default function Home() {
           />
         </div>
 
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="cv" className="text-3xl">CV:</label>
+          <input
+            id="cv"
+            className="block w-full p-2.5 text-2xl bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            type="file"
+            accept=".pdf"
+            onChange={handleCVChange}
+          />
+        </div>
 
         <button type="submit" className="bg-blue-500 text-white rounded-lg p-2">Enviar</button>
       </form>
