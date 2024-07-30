721dcf6d99172d733f94f150621085d6c71ac0c7
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-07-30 11:56:49 -0300

    Refactor: types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,13 +1,8 @@
-
-// Definición del tipo FormData
 export type FormData = {
   fullName: string;
   titles: string[];
   years: number;
   nacionality: string;
   linksSocialNetworks: Record<string, string>;
-  skills: Record<string, string[]>; // Cambiar aquí para que sea un Record
+  skills: Record<string, string[]>;
 };
-
-
-
